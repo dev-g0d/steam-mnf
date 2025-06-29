fd6c51d852f4f0644a58e5e8ad11c181d0f4b3be
dev-g0d <anonymouz.g0d@proton.me>
2025-06-29 19:54:39 +0700

    Add files via upload

---
+++ 1142100.lua
@@ -0,0 +1,26 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 1142100 Manifest
+-- Name: WWE 2K BATTLEGROUNDS
+-- Generated: 2025-06-08 03:11:21
+-- Total Depots: 3
+-- Total DLCs: 4
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1142100) -- WWE 2K BATTLEGROUNDS
+
+-- MAIN APP DEPOTS
+addappid(1142101, 1, "2fb5547f8c8e9b82c818cef8c908c4d1904b56a997e1a41c40c83a3a4e03ca1a") -- Elvis Content
+setManifestid(1142101, "7028484146642105155", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1363630) -- Edge Pack
+addappid(1363631) -- Superstar Pack
+addappid(1363632) -- 1100 VC Pack
+addappid(1544760) -- WWE 2K BATTLEGROUNDS - Ultimate Brawlers Pass
