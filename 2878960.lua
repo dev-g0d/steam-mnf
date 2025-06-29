b410d2177ad471a78a6c06c1f150fb19acadae6f
dev-g0d <anonymouz.g0d@proton.me>
2025-06-29 20:06:03 +0700

    Add files via upload

---
+++ 2878960.lua
@@ -0,0 +1,30 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 2878960 Manifest
+-- Name: WWE 2K25
+-- Generated: 2025-06-10 06:52:30
+-- Total Depots: 2
+-- Total DLCs: 10
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2878960) -- WWE 2K25
+
+-- MAIN APP DEPOTS
+addappid(2878961, 1, "06400ef21bacfd625a917c8964e13740059333d2173dc2496357ef23db97f483") -- Main Game Content (Windows Content)
+setManifestid(2878961, "3789709863833904805", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "550968249685141759", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3331100) -- WWE 2K25 Wyatt Sicks Pack
+addappid(3401590) -- WWE 2K25 Deadman Edition Bonus Pack
+addappid(3401600) -- WWE 2K25 MyFACTION Persona Card - The Rock (NoD)
+addappid(3401610) -- WWE 2K25 The Bloodline Edition Bonus Pack
+addappid(3401630) -- WWE 2K25 SuperCharger
+addappid(3401640) -- WWE 2K25 MyRISE Boost
+addappid(3401650) -- WWE 2K25 New Wave Pack
+addappid(3482630) -- WWE 2K25 Season Pass
+addappid(3482640) -- WWE 2K25 Ringside Pass
+addappid(3482860) -- WWE 2K25 Superstar Mega-Boost
