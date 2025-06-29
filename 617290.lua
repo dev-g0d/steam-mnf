8f5fb8d4fe9837f49674c9689f6d4f43362d468a
dev-g0d <anonymouz.g0d@proton.me>
2025-06-29 17:19:12 +0700

    Add files via upload

---
+++ 617290.lua
@@ -0,0 +1,31 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 617290 Manifest
+-- Name: Remnant: From the Ashes
+-- Generated: 2025-06-06 23:42:09
+-- Total Depots: 4
+-- Total DLCs: 2 (3 excluded)
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(617290) -- Remnant: From the Ashes
+
+-- MAIN APP DEPOTS
+addappid(617291, 1, "a44b1e94ad062cd59177b19472b97fbf016a18c93db2c7ab52857728012fa6ce") -- Remnant Content
+setManifestid(617291, "6473204158184554312", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+setManifestid(228986, "8782296191957114623", 0)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") -- VC 2017 Redist (Shared from App 228980)
+setManifestid(228987, "4302102680580581867", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1074850) -- Remnant From the Ashes pre-order bundle
+addappid(1344680) -- Remnant From the Ashes - Subject 2923
+
+-- DLCS EXCLUDED (NO DEPOT KEYS, NO DEDICATED DEPOTS)
+-- addappid(1120500) -- Remnant From the Ashes - Flight Team Cap (no keys available)
+-- addappid(1147300) -- Remnant From the Ashes - Bomber Hat (no keys available)
+-- addappid(1245150) -- Remnant From the Ashes - Swamps of Corsus (no keys available)
