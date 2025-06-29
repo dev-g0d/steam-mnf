e59281096ae9e0c06229de36edc982413629049b
dev-g0d <anonymouz.g0d@proton.me>
2025-06-29 19:59:49 +0700

    Add files via upload

---
+++ 1699480.lua
@@ -0,0 +1,26 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 1699480 Manifest
+-- Name: KLETKA
+-- Generated: 2025-06-27 21:55:06
+-- Total Depots: 4
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1699480) -- KLETKA
+
+-- MAIN APP DEPOTS
+addappid(1699481, 1, "c68d3843c14b9584d5c8707632ad7c1876efeb1528e0b97802b47663295ec466") -- Depot 1699481
+setManifestid(1699481, "1156018895642205553", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
+
+-- DLCS WITH DEDICATED DEPOTS
+-- KLETKA Soundtrack (AppID: 3376170)
+addappid(3376170)
+addappid(3376171, 1, "dfc3d945444d68cb1f4bca48d6b68dc7b814c0a6270d1105e51698db708c8d27") -- KLETKA Soundtrack - Depot 3376171
+setManifestid(3376171, "1850378261520941558", 0)
