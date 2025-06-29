026ec1e25887ba2335c926899b0ed20bd0bf09ad
dev-g0d <anonymouz.g0d@proton.me>
2025-06-29 19:55:47 +0700

    Add files via upload

---
+++ 1282100.lua
@@ -0,0 +1,33 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 1282100 Manifest
+-- Name: Remnant II
+-- Generated: 2025-06-06 23:42:13
+-- Total Depots: 3
+-- Total DLCs: 7
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1282100) -- Remnant II
+
+-- MAIN APP DEPOTS
+addappid(1282101, 1, "c44eaabd20d9d5479bea7e31ad0a7c0571d120c77b28814ed4175f6f790fdf7f") -- Main Game Content (Windows Content)
+setManifestid(1282101, "3402363259625448228", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2384070) -- Remnant II - Survival Pack
+addtoken(2384070, "6318262571818004518")
+addappid(2384071) -- Remnant II - DLC Bundle
+addtoken(2384071, "16903940029144554795")
+addappid(2384072) -- Remnant II - Gunslinger Unlock
+addtoken(2384072, "13725955972237952684")
+addappid(2384073) -- Remnant 2 - The Awakened King
+addappid(2384074) -- Remnant II - The Forgotten Kingdom
+addappid(2384075) -- Remnant II - The Dark Horizon
+addappid(2384076) -- Remnant II - Deluxe Edition Items
+addtoken(2384076, "10631820531645012218")
