4fe5c79ff132c8d6dc92b711a106ec99c383ef4f
dev-g0d <anonymouz.g0d@proton.me>
2025-06-29 20:00:52 +0700

    Add files via upload

---
+++ 1817070.lua
@@ -0,0 +1,47 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 1817070 Manifest
+-- Name: Marvel’s Spider-Man Remastered
+-- Generated: 2025-06-06 08:54:53
+-- Total Depots: 15
+-- Total DLCs: 1
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1817070) -- Marvel’s Spider-Man Remastered
+
+-- MAIN APP DEPOTS
+addappid(1817071, 1, "490a357029d1621deff9708e5d42f19324ccc758124ae1b455e091551df2a041") -- Main Game Content (Windows Content)
+setManifestid(1817071, "7101193785692308687", 0)
+addappid(1817072, 1, "ae2589c0e9de7a18dea92ceac08a6537cf7b83c1983e9f4738a188a49fd137f9") -- Game Content (Linux Binaries)
+setManifestid(1817072, "670672075597558132", 0)
+addappid(1817074, 1, "d1d379b15dba708a435fa177bacfc7f625c53c84b45b299ded3cbeeca084e330") -- Common Library Files (Content)
+setManifestid(1817074, "2767115330843299188", 0)
+addappid(1817075, 1, "ecb149a90bf5eda310dd5f18e7715c255b022db3e327037cc6daa4f69ff22107") -- Main Game Content (Depot 1817075)
+setManifestid(1817075, "3039496220919935469", 0)
+addappid(1817076, 1, "6cb4d05622fdd2cd0d10703a36cf47140a2d952a49e31325a3b65af53db59974") -- Main Game Content (Depot 1817076)
+setManifestid(1817076, "7023492310614800670", 0)
+addappid(1817077, 1, "3e7903be905c0a12045d1f7d303e596ca9889e64b0b2f4a5e52680e7839b20fb") -- Main Game Content (Depot 1817077)
+setManifestid(1817077, "1086775484848552274", 0)
+addappid(1817078, 1, "9815d29285202efd418b703002a67cf17ccde07230fe3f2b171e1581d17ba0e4") -- Main Game Content (Depot 1817078)
+setManifestid(1817078, "2990142445242902328", 0)
+addappid(1817079, 1, "e357c3433e694ca3c616af966efc889f3d6af51c4d45e34fbf3d6537715c9bf0") -- Main Game Content (Depot 1817079)
+setManifestid(1817079, "2023143758263501793", 0)
+addappid(1829890, 1, "fc8ce2606acf5525e88dffc6cdc9cd4cf50e092efa04536528ee66fda012fe79") -- Game Content (Depot 1829890)
+setManifestid(1829890, "3912377727024202284", 0)
+addappid(1829891, 1, "69db276eece234ce4caf8b55262d17c74686450104a1d1db09cfdc425555b707") -- Windows Content (Depot 1829891)
+setManifestid(1829891, "2632761098204669985", 0)
+addappid(1829892, 1, "cd4dd166b577600a9fe435ad640a58caa41fb8311111ab403c24a7b1d5a29ed9") -- Linux Content (Depot 1829892)
+setManifestid(1829892, "1540729933509716096", 0)
+addappid(1829893, 1, "48c0acb19e2fc2d02db4b9dcf88fabf25c8eb7eb1e218b7f26c54a53073bb934") -- Mac Content (Depot 1829893)
+setManifestid(1829893, "5021811276708038952", 0)
+addappid(1829894, 1, "1406f2b316ca60231c614560afc407a0b3eca5c0cce4e46f5493e73f269808ef") -- Game Content (Depot 1829894)
+setManifestid(1829894, "2167297873910007583", 0)
+addappid(1829895, 1, "4773012855296205ad41eeb15231c6d5db4ee4264feb5b217e8b09b752c4526c") -- Game Content (Depot 1829895)
+setManifestid(1829895, "4268839994794210418", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2083110) -- Marvels Spider-Man Remastered - Pre-purchase Entitlements
