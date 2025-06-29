4162b4a2dbb15406e7997068de1d4f019fcd76de
dev-g0d <anonymouz.g0d@proton.me>
2025-06-29 19:54:12 +0700

    Add files via upload

---
+++ 728880.lua
@@ -0,0 +1,25 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 728880 Manifest
+-- Name: Overcooked! 2
+-- Generated: 2025-06-06 16:54:59
+-- Total Depots: 3
+-- Total DLCs: 6
+
+-- MAIN APPLICATION
+addappid(728880) -- Overcooked! 2
+
+-- MAIN APP DEPOTS
+addappid(728881, 1, "6ab9f32746295a38fc0a75db089afc2394e0ced25ca436201027ed360890e5bc") -- Balloon Windows
+setManifestid(728881, "8562236934291228162", 0)
+addappid(728882, 1, "3693ed13535f104258a7425e20b9e1fae3f30ec4d78a09f0b89cd55350991d51") -- Balloon Linux
+setManifestid(728882, "433550811082520169", 0)
+addappid(728883, 1, "67ac3a681ded025499180d68f3753ce53f4934976c75af86ab9a6287056dcdae") -- Balloon OSX
+setManifestid(728883, "604448138896078557", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(858240) -- Overcooked 2 - Too Many Cooks Pack
+addappid(909720) -- Overcooked 2 - Surf n Turf
+addappid(1013300) -- Overcooked 2 - Campfire Cook Off
+addappid(1017510) -- Overcooked 2 - Night of the Hangry Horde
+addappid(1067770) -- Overcooked 2 - Season Pass
+addappid(1138400) -- Overcooked 2 - Carnival of Chaos
