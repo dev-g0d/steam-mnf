99bfdeff869d81686942df6a202662e297ff62bc
dev-g0d <anonymouz.g0d@proton.me>
2025-06-29 20:04:01 +0700

    Add files via upload

---
+++ 2404880.lua
@@ -0,0 +1,13 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 2404880 Manifest
+-- Name: Car Dealer Simulator
+-- Generated: 2025-06-27 11:21:05
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2404880) -- Car Dealer Simulator
+
+-- MAIN APP DEPOTS
+addappid(2404881, 1, "dd3322cbddd81741e4bd8f3d9d08fbe6ff172e3f5c36fec42e215f6661d3ea4f") -- Depot 2404881
+setManifestid(2404881, "926981093506056616", 0)
