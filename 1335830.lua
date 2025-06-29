805cd20b84d3b96fe25de18a98018f2e2f303d42
dev-g0d <anonymouz.g0d@proton.me>
2025-06-29 20:34:12 +0700

    Add files via upload

---
+++ 1335830.lua
@@ -0,0 +1,27 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 1335830 Manifest
+-- Name: Len's Island
+-- Generated: 2025-06-27 21:10:19
+-- Total Depots: 4
+-- Total DLCs: 3
+
+-- MAIN APPLICATION
+addappid(1335830) -- Len's Island
+
+-- MAIN APP DEPOTS
+addappid(1335831, 1, "f2232a3bfcb5c7612c95312dfd25d89e6f0795121c0554e12b911b6f8d982e65") -- Len's Island Content
+setManifestid(1335831, "4731784968860703882", 0)
+addappid(1335832, 1, "3415b4a91f32415894d413bde36a9241af420679c00e1b11fb96c0c8ad90dcbd") -- Len's Island Mac Content
+setManifestid(1335832, "923412842648711394", 0)
+
+-- DLCS WITH DEDICATED DEPOTS
+-- Lens Island Soundtrack (AppID: 1787330)
+addappid(1787330)
+addappid(1787331, 1, "9d405e1bee21e52096a43769ff856cb522150a42fa61da576d6635688b0e6d45") -- Lens Island Soundtrack - Len's Island Soundtrack Content
+setManifestid(1787331, "7376270915747671981", 0)
+addappid(1787332, 1, "b51155a28fc7cebcbc45e30d2c6d9f15c26b27d9d12def30b617e3ddd3a5e555") -- Lens Island Soundtrack - High Quality Audio
+setManifestid(1787332, "331110360578742451", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3795980) -- Lens Island - Gothic Building Style
+addappid(3809990) -- Lens Island - Supporter Pack
