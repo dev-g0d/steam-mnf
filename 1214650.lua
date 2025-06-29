940792d075b3c9a4f6fb86e74f7deca9f4f0d9da
dev-g0d <anonymouz.g0d@proton.me>
2025-06-29 19:55:03 +0700

    Add files via upload

---
+++ 1214650.lua
@@ -0,0 +1,46 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 1214650 Manifest
+-- Name: SOUTH PARK: SNOW DAY!
+-- Generated: 2025-06-07 06:48:38
+-- Total Depots: 5
+-- Total DLCs: 15
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1214650) -- SOUTH PARK: SNOW DAY!
+
+-- MAIN APP DEPOTS
+addappid(1214651, 1, "ab40da96e959d0113c4a52d5736cce431bb07459a9a22b60d227598aa1420aa3") -- Main Game Content (Windows Content)
+setManifestid(1214651, "5679390413065553070", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
+
+-- DLCS WITH DEDICATED DEPOTS
+-- SOUTH PARK SNOW DAY - Underpants Gnome Cosmetics Pack (AppID: 2534280)
+addappid(2534280)
+addappid(2534280, 1, "499001d339eb0c50bf4c3608fc07a306521cdd526a41a06a3cd53578e5cbcd0b") -- SOUTH PARK SNOW DAY - Underpants Gnome Cosmetics Pack - Main Content
+setManifestid(2534280, "5325105245187432134", 0)
+
+-- SOUTH PARK SNOW DAY - To Danse with Ravenous Shadows (AppID: 2777700)
+addappid(2777700)
+addappid(2777700, 1, "f43acf1097fae487007d0fdc5825a2c8fd61b9cb8bd6c407181a3a09a09913af") -- SOUTH PARK SNOW DAY - To Danse with Ravenous Shadows - Main Content
+setManifestid(2777700, "8996314653308100215", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2717700) -- SOUTH PARK SNOW DAY - Season Pass
+addappid(2777690) -- SOUTH PARK SNOW DAY - Snowball
+addappid(2856250) -- SOUTH PARK SNOW DAY - Bear-serker Pack
+addappid(2856260) -- SOUTH PARK SNOW DAY - CRED Pack
+addappid(2895790) -- SOUTH PARK SNOW DAY - Asspen Pack
+addappid(2904870) -- SOUTH PARK SNOW DAY - 420 Pack
+addappid(3000090) -- SOUTH PARK SNOW DAY - To Danse with the Veiled Horde
+addappid(3013230) -- SOUTH PARK SNOW DAY - Nonconformist Weapon Variation Pack
+addappid(3053410) -- SOUTH PARK SNOW DAY - Bigger, Longer  Uncut Anniversary Pack
+addappid(3126390) -- SOUTH PARK SNOW DAY - Hammer
+addappid(3126400) -- SOUTH PARK SNOW DAY - Good Times with Weapons - Weapon Variation Pack
+addappid(3223240) -- SOUTH PARK SNOW DAY - Nicholes Home Brew
+addappid(3312150) -- SOUTH PARK SNOW DAY - Chaos Pack
