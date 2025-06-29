4cbdf7e58e4c50c7a5b1e469c4239bd9af3f5669
dev-g0d <anonymouz.g0d@proton.me>
2025-06-29 19:53:42 +0700

    Add files via upload

---
+++ 688540.lua
@@ -0,0 +1,20 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 688540 Manifest
+-- Name: ZOMBIE RAID: No One Survives
+-- Generated: 2025-06-08 04:21:45
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(688540) -- ZOMBIE RAID: No One Survives
+
+-- MAIN APP DEPOTS
+addappid(688541, 1, "4ab92f086dd11ccdda52175de5301341c928d8b2739401c43b4da0732019a99e") -- Main Game Content (Windows Content)
+setManifestid(688541, "3970568590459505957", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
