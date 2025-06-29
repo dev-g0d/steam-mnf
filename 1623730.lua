a7b5938eb7bd19a57aa9fed067267df6ba2e6ad8
dev-g0d <anonymouz.g0d@proton.me>
2025-06-29 19:57:25 +0700

    Add files via upload

---
+++ 1623730.lua
@@ -0,0 +1,28 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 1623730 Manifest
+-- Name: Palworld
+-- Generated: 2025-06-25 05:24:20
+-- Total Depots: 5
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1623730) -- Palworld
+
+-- MAIN APP DEPOTS
+addappid(1623731, 1, "7a422988135296505205dd489945d4b17ed7414734dd627744adac84aade7b73") -- Depot 1623731
+setManifestid(1623731, "7704907311656361483", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
+
+-- DLCS WITH DEDICATED DEPOTS
+-- Palworld - Soundtrack (AppID: 2771110)
+addappid(2771110)
+addappid(2771111, 1, "802dd0505be5518464fe77ed7afa2c28611463bf00ec5ef400643b323d09c4c7") -- Palworld - Soundtrack - Depot 2771111
+setManifestid(2771111, "6472366291146846212", 0)
+addappid(2771112, 1, "c385e52ae8ad3b8756ab3caa03e50b105c0157fb797bb5acbff1bbb8bb27692f") -- Palworld - Soundtrack - Depot 2771112
+setManifestid(2771112, "4099416431916212746", 0)
