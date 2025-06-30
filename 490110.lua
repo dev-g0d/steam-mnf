344e4bbf93819be4e93a89d406873ef1dab3d928
dev-g0d <anonymouz.g0d@proton.me>
2025-06-30 18:36:01 +0700

    Add files via upload

---
+++ 490110.lua
@@ -0,0 +1,18 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 490110 Manifest
+-- Name: The Precinct
+-- Generated: 2025-06-25 13:58:30
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(490110) -- The Precinct
+
+-- MAIN APP DEPOTS
+addappid(490111, 1, "44bcc3870bf156385e37ff47e71ae28824fa905a542cdc8eaf05715799f78fc2") -- Depot 490111
+setManifestid(490111, "5298644583418093684", 0)
+
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- The Precinct Soundtrack (AppID: 3240070) - missing depot keys
+-- addappid(3240070)
